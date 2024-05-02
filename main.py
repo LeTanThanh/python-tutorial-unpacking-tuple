3ce6dc92da828977ac4dbf684f7ae324ea2571db
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2024-05-02 15:08:24 +0700

    Reviewing Python tuples

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
 if __name__ == "__main__":
-  print("Python Unpacking Tuple")
+  # Reviewing Python tuples
+
+  example_tuple = 1, 2
+  print(example_tuple)
+
+  example_tuple = (1, 2)
+  print(example_tuple)
+
+  example_tuple = ()
+  print(example_tuple)
+
+  example_tuple = tuple()
+  print(example_tuple)
+
+  example_tuple = 1,
+  print(example_tuple)
+
+  example_tuple = (1,)
+  print(example_tuple)
